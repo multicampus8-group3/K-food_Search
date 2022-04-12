267e0533b2387bc3a2ea9a9bb1bf38dc1c2ad9c0
didwltjr93 <didwltjr93@gmail.com>
2022-04-12 17:49:28 +0900

    잘 돌아가는거 (약간 수정한거) 커밋

--- k-food_search/src/main/java/com/campus/myapp/HomeController.java
+++ k-food_search/src/main/java/com/campus/myapp/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 	@RequestMapping("/")
 	public String Home() {
-		return "home_main";
+		//return "home_main";
+		return "mango_test/mango_test";
 	}
 }
